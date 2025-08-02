4cfaa9b9510271fe0ff0594dc559fda8b56e849b
Keith Trnka <keith.trnka@gmail.com>
2025-08-01 20:27:26 -0700

    Cleanup test data

--- tests/etl/data/download_examples.sh
+++
@@ -1,6 +0,0 @@
-curl -L 'https://seattle.greencitypartnerships.org/event/calendar/' \
-     -o tests/fixtures/gsp_calendar.html
-curl -L 'https://www.trumba.com/calendars/volunteer-1.rss' \
-     -o tests/fixtures/spr_volunteer.rss
-curl -L 'https://www.seattleparksfoundation.org/events/' \
-     -o tests/fixtures/spf_events.html
